5cc9c89f56b01747e6bd86839c0203be812630df
Nathan Esquenazi <nesquena@gmail.com>
2012-12-27 18:32:55 -0800

    Commits working basic snake game for ex5

--- src/GameExercises.java
+++ src/GameExercises.java
@@ -1,49 +1,123 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 
 @SuppressWarnings("serial")
 public class GameExercises extends GamePanel {	
-	
+
 	// This method is called once a second to redraw the canvas,
 	// so you can do things like animate the snake.
 	public void redrawCanvas(Graphics canvas) {
 		drawLabel(canvas);
 		drawApple(canvas);
-	    drawSnake(canvas, size, x, y);
+		drawSnake(canvas, size, x, y);
 	}
-	
+
 	// This method is called once a second, and it is a good place to
 	// do things like check if the snake has collided into the wall
 	// or the apple.
 	public void onTimer() {
-		moveUp(x, y);
+		if (isGameRunning) {
+			checkApple();
+			checkBoundsCollision();
+			moveSnake(x, y);
+		}	
 	}
 	
+	// This method is called whenever a keyboard button is pressed
+	// within your game. The keyCode represents the key the actual key pushed.
+	// You can check which keyCode using 'KeyEvent' constants.
+	// i.e keyCode == KeyEvent.VK_G
+	public void onKeyPress(int keyCode) {
+		if ((keyCode == KeyEvent.VK_LEFT) && !(direction == "right")) {
+			direction = "left";
+		}
+		else if ((keyCode == KeyEvent.VK_RIGHT) && !(direction == "left")) {
+			direction = "right";
+		}
+		else if ((keyCode == KeyEvent.VK_UP) && !(direction == "down")) {
+			direction = "up";
+		}
+		else if ((keyCode == KeyEvent.VK_DOWN) && !(direction == "up")) {
+			direction = "down";
+		} 
+	}
+	
+	// Moves the coordinates for snake up
+	public void moveSnake(int[] x, int[] y) {
+		// First since a snake slides across the tiles,
+		// move each tile (except the head) so that every tile is now equal
+		// to the previous tile (to move all tiles).
+		for (int ind = snakeLength; ind > 0; ind--) {
+			x[ind] = x[ind - 1];
+			y[ind] = y[ind - 1];
+		}
+
+		// Based on the current direction, we need to
+		// move the head tile towards the next tile in that direction...
+		if (direction == "left") {
+			x[0] -= size;
+		} else if (direction == "right") {
+			x[0] += size;
+		} else if (direction == "up") {
+			y[0] -= size;
+		} else if (direction == "down") {
+			y[0] += size;
+		}
+	}
+	
+	private void checkBoundsCollision() {
+		// Check if the snake ran into itself
+		for (int ind = snakeLength; ind > 0; ind--) {
+			if ((x[0] == x[ind]) && (y[0] == y[ind])) {
+				isGameRunning = false;
+			}
+		}
+		
+		// Check if snake ran into the boundaries of the game board
+		// Game is over if the snake head are outside board bounds
+		if (x[0] < 0 || x[0] > getPanelWidth()) {
+			isGameRunning = false;
+		} else if (y[0] < 0 || y[0] > getPanelHeight()) {
+			isGameRunning = false;
+		}
+	}
+
+	private void checkApple() {
+		// When snake head is directly on the apple tile,
+		// apple is then consumed
+		if ((x[0] == appleX) && (y[0] == appleY)) {
+			snakeLength++;
+			x = createArrayFromOldArrayWithSize(x, x.length + 1);
+			y = createArrayFromOldArrayWithSize(y, y.length + 1);
+			repositionApple();
+		}		
+	}
+
+	private void repositionApple() {
+		appleX = randomCoordForPanel(getPanelWidth());
+		appleY = randomCoordForPanel(getPanelHeight());
+	}
+
 	public void drawLabel(Graphics canvas) {
 		canvas.setColor(Color.BLUE);
-	    Font f = new Font("Helvetica Nueue", Font.BOLD, 24);
+		Font f = new Font("Helvetica Nueue", Font.BOLD, 24);
 		canvas.setFont(f);
-	    canvas.drawString("Exercise 3", 50, 50);
+		canvas.drawString("Exercise 5", 50, 50);
 	}
-	
+
 	public void drawApple(Graphics canvas) {
 		canvas.setColor(Color.RED);
-	    canvas.fillOval(150, 150, 25, 25);
+		canvas.fillOval(appleX, appleY, 25, 25);
 	}
-	
+
 	public void drawSnake(Graphics canvas, int size, int[] x, int[] y) {
 		Color c = Color.GREEN;
 		canvas.setColor(c);
 		for (int ind = 0; ind < x.length; ind++) {
-	      canvas.fillRect(x[ind], y[ind], size, size);
+			canvas.fillRect(x[ind], y[ind], size, size);
 		}
 	}
-	
-	// Moves the coordinates for snake up
-	public void moveUp(int[] x, int[] y) {
-		for (int ind = 0; ind < x.length; ind++) {
-			y[ind] = y[ind] - size;
-		}
-	}
+
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,12 @@
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Arrays;
+import java.util.Random;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -8,12 +14,21 @@ import javax.swing.Timer;
 
 @SuppressWarnings("serial")
 public abstract class GamePanel extends JPanel implements ActionListener {
+	// Sets the width and height of the canvas to 300x300
+	private final int WIDTH = 600;
+	private final int HEIGHT = 600;
+	
 	int[] x;
 	int[] y;
 	int size = 25;
 	int xStart = 300;
-    int yStart = 300;
-	
+	int yStart = 300;
+	String direction;
+	int snakeLength;
+	int appleX;
+	int appleY;
+	boolean isGameRunning;
+
 	// Creates a timer which 'ticks' and will trigger
 	// the actionPerformed method every X interval
 	// This is how the game progresses, the snake moves in 
@@ -21,28 +36,57 @@ public abstract class GamePanel extends JPanel implements ActionListener {
 	private Timer timer;
 
 	public GamePanel() {
-	    x = new int[] { xStart, xStart, xStart, xStart };
-	    y = new int[] { yStart, yStart + size + 1, 
-	    		yStart + (size * 2) + 2, 
-	    		yStart + (size * 3) + 3 };
+		setFocusable(true);
+		snakeLength = 3;
+		direction = "up";
+		x = new int[] { xStart, xStart, xStart, xStart };
+		y = new int[] { yStart, yStart + size, 
+				yStart + (size * 2), 
+				yStart + (size * 3) };
+		appleX = 150;
+		appleY = 150;
+		
+		addKeyListener(new GameKeyListener());
 		timer = new Timer(150, this);
 		startGame();
 	}
-	
+
 	protected void startGame() {
+		isGameRunning = true;
 		timer.start();
 	}
-	
+
 	protected void pauseGame() {
 		timer.stop();
+	}
+	
+	protected int getPanelHeight() {
+		return HEIGHT;
+	}
+	
+	protected int getPanelWidth() {
+		return WIDTH;
+	}
+	
+	protected int randomCoordForPanel(int max) {
+		int multiplier = max / size;
+		int randomCoordinate = (int) (Math.random() * multiplier);
+		return randomCoordinate * size;
+	}
+	
+	protected boolean hasObjectCollision(int x1, int y1, int x2, int y2) {
+		Rectangle rx1 = new Rectangle(x1, y1, size, size);
+	    Rectangle rx2 = new Rectangle(x2, y2, size, size);
+	    return rx1.intersects(rx2);
 	}
 
 	// Draws every time the component is painted
 	// This contains the contents of the frame
 	public void paintComponent(Graphics canvas) {	
 		super.paintComponent(canvas);
-		
 		this.redrawCanvas(canvas);
+		Toolkit.getDefaultToolkit().sync();
+		canvas.dispose();
 	}
 
 	// This fires for every 'tick' of the timer
@@ -56,4 +100,19 @@ public abstract class GamePanel extends JPanel implements ActionListener {
 
 	protected abstract void onTimer();
 	protected abstract void redrawCanvas(Graphics canvas);
+	protected abstract void onKeyPress(int keyCode);
+
+	// LISTENER
+	private class GameKeyListener extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			onKeyPress(e.getKeyCode());
+		}
+	}
+	
+	// Arrays
+	// createArrayFromOldArrayWithSize(oldArray, 10);
+	int[] createArrayFromOldArrayWithSize(int[] org, int size) {
+	    int[] result = Arrays.copyOf(org, size);
+	    return result;
+	}
 }
