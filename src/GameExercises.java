b259dbe7d36159ab9c69a2e6f911cdf481ffd7fe
Nathan Esquenazi <nesquena@gmail.com>
2012-12-27 16:04:12 -0800

    Adds answer to exercise 1

--- src/GameExercises.java
+++ src/GameExercises.java
@@ -4,6 +4,15 @@ import java.awt.Graphics;
 public class GameExercises extends GamePanel {
 	// Implement the drawing here
 	public void paintComponent(Graphics canvas) {	
-
+		// We want to draw a red circle (apple), a green square (snake head)
+		// and blue text which reads "Exercise 1"
+		canvas.setColor(Color.RED);
+	    canvas.fillOval(150, 150, 100, 100);
+	    canvas.setColor(Color.GREEN);
+	    canvas.fillRect(400, 350, 100, 100);
+	    canvas.setColor(Color.BLUE);
+	    Font f = new Font("Helvetica Nueue", Font.BOLD, 24);
+		canvas.setFont(f);
+	    canvas.drawString("Exercise 1", 50, 50);
 	}
 }
