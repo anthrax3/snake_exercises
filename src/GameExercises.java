a841a3504364076bb018a1982a2e81d8c759ebd4
Nathan Esquenazi <nesquena@gmail.com>
2012-12-27 19:20:25 -0800

    Adds first game panel

--- src/GameExercises.java
+++ src/GameExercises.java
@@ -3,14 +3,14 @@ import java.awt.Font;
 import java.awt.Graphics;
 
 @SuppressWarnings("serial")
-public class GameExercises extends GamePanel {	
+public class GameExercises extends FirstGamePanel {	
 	
 	// This method is called once a second to redraw the canvas,
 	// so you can do things like animate the snake.
 	public void redrawCanvas(Graphics canvas) {
 		drawLabel(canvas);
 		drawApple(canvas);
-	    drawSnake(canvas, size, x, y);
+	    drawSnake(canvas, drawSize, x, y);
 	}
 	
 	// This method is called once a second, and it is a good place to
@@ -18,6 +18,14 @@ public class GameExercises extends GamePanel {
 	// or the apple.
 	public void onTimer() {
 		moveUp(x, y);
+	}
+	
+	// This method is called whenever a keyboard button is pressed
+	// within your game. The keyCode represents the key the actual key pushed.
+	// You can check which keyCode using 'KeyEvent' constants.
+	// i.e keyCode == KeyEvent.VK_G
+	protected void onKeyPress(int keyCode) {
+		
 	}
 	
 	public void drawLabel(Graphics canvas) {
@@ -32,20 +40,21 @@ public class GameExercises extends GamePanel {
 	    canvas.fillOval(150, 150, 25, 25);
 	}
 	
-	public void drawSnake(Graphics canvas, int size, int[] x, int[] y) {
+	public void drawSnake(Graphics canvas, int drawSize, int[] x, int[] y) {
 		Color c = Color.GREEN;
 		canvas.setColor(c);
-	    canvas.fillRect(x[0], y[0], size, size);
-	    canvas.fillRect(x[1], y[1], size, size);
-	    canvas.fillRect(x[2], y[2], size, size);
-	    canvas.fillRect(x[3], y[3], size, size);
+	    canvas.fillRect(x[0], y[0], drawSize, drawSize);
+	    canvas.fillRect(x[1], y[1], drawSize, drawSize);
+	    canvas.fillRect(x[2], y[2], drawSize, drawSize);
+	    canvas.fillRect(x[3], y[3], drawSize, drawSize);
 	}
 	
 	// Moves the coordinates for snake up
 	public void moveUp(int[] x, int[] y) {
-		y[3] = y[3] - size;
-		y[2] = y[2] - size;
-		y[1] = y[1] - size;
-		y[0] = y[0] - size;
+		y[3] = y[3] - drawSize;
+		y[2] = y[2] - drawSize;
+		y[1] = y[1] - drawSize;
+		y[0] = y[0] - drawSize;
 	}
+
 }
