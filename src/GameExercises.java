f0e8e3b200a983c2f2407493b53b72609e3b63b6
Nathan Esquenazi <nesquena@gmail.com>
2012-12-27 16:02:58 -0800

    Move text into different exercises, adds basic frame

--- src/GameExercises.java
+++ src/GameExercises.java
@@ -1,3 +1,13 @@
-public class GameExercises {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class GameExercises extends JPanel {
+	// Implement the drawing here
+	public void paintComponent(Graphics canvas) {	
+
+	}
 }
