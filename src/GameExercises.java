b02bfdf04f9094f2ed29251a3c9e98f5168390e2
Nathan Esquenazi <nesquena@gmail.com>
2012-12-27 19:27:11 -0800

    Adds first game panel

--- src/GameExercises.java
+++ src/GameExercises.java
@@ -1,9 +1,24 @@
 import java.awt.Graphics;
 
 @SuppressWarnings("serial")
-public class GameExercises extends GamePanel {
+public class GameExercises extends FirstGamePanel {
 	// Implement the drawing here
-	public void paintComponent(Graphics canvas) {	
+	public void redrawCanvas(Graphics canvas) {	
+
+	}
+	
+	// This method is called once a second, and it is a good place to
+	// do things like check if the snake has collided into the wall
+	// or the apple.
+	public void onTimer() {
+
+	}
+
+	// This method is called whenever a keyboard button is pressed
+	// within your game. The keyCode represents the key the actual key pushed.
+	// You can check which keyCode using 'KeyEvent' constants.
+	// i.e keyCode == KeyEvent.VK_G
+	protected void onKeyPress(int keyCode) {
 
 	}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,39 +1,101 @@
+
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Arrays;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 
 @SuppressWarnings("serial")
-public class GamePanel extends JPanel implements ActionListener {
-	// Creates a timer which 'ticks' and will trigger
-	// the actionPerformed method every X interval
-	// This is how the game progresses, the snake moves in 
-	// the specified direction every 'tick' of the timer.
-	private Timer timer;
+public abstract class GamePanel extends JPanel implements ActionListener {
+  // Sets the width and height of the canvas to 600x600
+  private final int WIDTH = 600;
+  private final int HEIGHT = 600;
 
-	public GamePanel() {
-		timer = new Timer(150, this);
-	}
-	
-	protected void startGame() {
-		timer.start();
-	}
+  int drawSize = 25;
+  boolean isGameRunning;
 
-	// Draws every time the component is painted
-	// This contains the contents of the frame
-	public void paintComponent(Graphics canvas) {	
+  // Creates a timer which 'ticks' and will trigger
+  // the actionPerformed method every X interval
+  // This is how the game progresses, the snake moves in
+  // the specified direction every 'tick' of the timer.
+  private Timer timer;
 
-	}
+  public GamePanel() {
+    setFocusable(true);
+    addKeyListener(new GameKeyListener());
+    timer = new Timer(150, this);
+    startGame();
+  }
 
-	// This fires for every 'tick' of the timer
-	// which means every specified interval. When this fires,
-	// we should move the game forward which means checking
-	// conditions (apple hit or game over) and moving the snake.
-	public void actionPerformed(ActionEvent e) {
+  protected void startGame() {
+    isGameRunning = true;
+    timer.start();
+  }
 
-	}
+  protected void pauseGame() {
+    timer.stop();
+  }
 
+  protected int getPanelHeight() {
+    return HEIGHT;
+  }
+
+  protected int getPanelWidth() {
+    return WIDTH;
+  }
+
+  protected int randomCoordForPanel(int max) {
+    int multiplier = max / drawSize;
+    int randomCoordinate = (int) (Math.random() * multiplier);
+    return randomCoordinate * drawSize;
+  }
+
+  protected boolean hasObjectCollision(int x1, int y1, int x2, int y2) {
+    Rectangle rx1 = new Rectangle(x1, y1, drawSize, drawSize);
+      Rectangle rx2 = new Rectangle(x2, y2, drawSize, drawSize);
+      return rx1.intersects(rx2);
+  }
+
+  // Draws every time the component is painted
+  // This contains the contents of the frame
+  public void paintComponent(Graphics canvas) {
+    super.paintComponent(canvas);
+    this.redrawCanvas(canvas);
+    Toolkit.getDefaultToolkit().sync();
+    canvas.dispose();
+  }
+
+  // This fires for every 'tick' of the timer
+  // which means every specified interval. When this fires,
+  // we should move the game forward which means checking
+  // conditions (apple hit or game over) and moving the snake.
+  public void actionPerformed(ActionEvent e) {
+    onTimer();
+    repaint();
+  }
+
+  protected abstract void onTimer();
+  protected abstract void redrawCanvas(Graphics canvas);
+  protected abstract void onKeyPress(int keyCode);
+
+  // LISTENER
+  private class GameKeyListener extends KeyAdapter {
+    public void keyPressed(KeyEvent e) {
+      onKeyPress(e.getKeyCode());
+    }
+  }
+
+  // Arrays
+  // createArrayFromOldArrayWithSize(oldArray, 10);
+  int[] createArrayFromOldArrayWithSize(int[] org, int size) {
+      int[] result = Arrays.copyOf(org, size);
+      return result;
+  }
 }
