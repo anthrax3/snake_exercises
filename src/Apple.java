b81c32be5ad2c527c34ca75f04f0ba75934af852
Nathan Esquenazi <nesquena@gmail.com>
2012-12-27 21:29:18 -0800

    Comment apple and snake classes

--- src/Apple.java
+++ src/Apple.java
@@ -3,29 +3,35 @@ import java.awt.Graphics;
 import java.awt.Point;
 
 public class Apple {
+	// This is the size of the apple to draw
 	private int drawSize;
+	// This is the coordinate position of the apple
 	private Point pos;
 	
 	public Apple(int size) {
 		this.pos = new Point(150, 150);
 		this.drawSize = size;
 	}
-
+	
+	// Returns the x coordinate position for the apple
 	public int getPosX() {
 		return pos.x;
 	}	
 	
+	// Returns the y coordinate position for the apple
 	public int getPosY() {
 		return pos.y;
-	}	
+	}
 	
+	// Update the position for the apple given the x and y coordinates
+	public void setPos(int x, int y) {
+		pos = new Point(x, y);
+	}
+	
+	// Given the canvas, draw the item on screen using coordinates
 	public void draw(Graphics canvas) {
 		canvas.setColor(Color.RED);
 		canvas.fillOval(getPosX(), getPosY(), drawSize, drawSize);
-	}
-
-	public void setPos(int x, int y) {
-		pos = new Point(x, y);
 	}
 	
 }
--- src/Snake.java
+++ src/Snake.java
@@ -4,11 +4,15 @@ import java.awt.Point;
 import java.util.Arrays;
 
 public class Snake {
+	// This is the size of the apple to draw
 	private int drawSize;
+	// The number of squares that make up this snake
 	private int length;
+	// The list of coordinates of the snake's body
 	private Point[] pos;
+	// The direction of the snake (left, right, up, down)
 	private String direction;
-	
+
 	public Snake(int size) {
 		this.direction = "up";
 		this.length = 3;
@@ -20,15 +24,41 @@ public class Snake {
 			pind.y = 300 + (ind * drawSize);
 		}
 	}
-	
+
+	// Returns the x coordinate position of the snake's head
 	public int getHeadPosX() {
 		return pos[0].x;
 	}
-	
+
+	// Returns the y coordinate position of the snake's head
 	public int getHeadPosY() {
 		return pos[0].y;
 	}
-	
+
+	// Increases the length of the snake's tail by 1
+	// Used to make the snake longer once an apple is eaten
+	// Needs to increment length, and instantiate a new array of a larger size
+	public void growTail() {
+		length++;
+		pos = Arrays.copyOf(pos, pos.length + 1);
+		pos[pos.length - 1] = new Point();
+	}
+
+	// Returns the direction the snake is headed
+	// Direction is up, down, left or right
+	public String getDirection() {
+		return direction;
+	}
+
+	// Updates the direction the snake is headed
+	// Accepts up, down, left or right
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+	// Returns true if the snake's head has hit the body
+	// This is a game over condition when the head runs into any part of the body
+	// We need to check for a collision
 	public boolean hasHitTail() {
 		for (int ind = length; ind > 0; ind--) {
 			if ((pos[0].x == pos[ind].x) && (pos[0].y == pos[ind].y)) {
@@ -37,21 +67,9 @@ public class Snake {
 		}
 		return false;
 	}
-	
-	public void growTail() {
-		length++;
-		pos = Arrays.copyOf(pos, pos.length + 1);
-		pos[pos.length - 1] = new Point();
-	}
 
-	public String getDirection() {
-		return direction;
-	}
-
-	public void setDirection(String direction) {
-		this.direction = direction;
-	}
-	
+	// Moves the snake in the direction he is moving.
+	// Needs to move the tail in the direction and adjust the head.
 	public void move() {
 		// First since a snake slides across the tiles,
 		// move each tile (except the head) so that every tile is now equal
@@ -73,7 +91,8 @@ public class Snake {
 			pos[0].y += drawSize;
 		}
 	}
-	
+
+	// Given the canvas, draw the snake on screen using known coordinates
 	public void draw(Graphics canvas) {
 		Color c = Color.GREEN;
 		canvas.setColor(c);
