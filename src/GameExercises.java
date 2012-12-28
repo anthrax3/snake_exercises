6d2673b320a1c2fde537c303dd712ca3c7b2cec5
Nathan Esquenazi <nesquena@gmail.com>
2012-12-27 16:28:54 -0800

    Adds first pass with draw snake

--- src/GameExercises.java
+++ src/GameExercises.java
@@ -12,42 +12,34 @@ public class GameExercises extends GamePanel {
 	    canvas.setColor(Color.BLUE);
 	    Font f = new Font("Helvetica Nueue", Font.BOLD, 24);
 		canvas.setFont(f);
-	    canvas.drawString("Exercise 2", 50, 50);
-
-	    // Simple graphic (draw the snake without loops)
-	    // First, draw manually, repeating colors and width
-	    /* 
-	    canvas.setColor(Color.GREEN);
-	    canvas.fillRect(300, 300, 25, 25);
-	    canvas.fillRect(300, 326, 25, 25);
-	    canvas.fillRect(300, 352, 25, 25);
-	    */
+	    canvas.drawString("Exercise 3", 50, 50);
 	    
-	    // Then, try to change the color and width of the snake
-	    // Refactor code so that changing the color and width is easier
-	    Color c = Color.GREEN;
+	    // Keep the array and variables from exercise 2
 	    int size = 25;
 	    int xStart = 300;
 	    int yStart = 300;
-	    
-	    /*
-	    canvas.setColor(c);
-	    canvas.fillRect(xStart, yStart, size, size);
-	    canvas.fillRect(xStart, yStart + size + 1, size, size);
-	    canvas.fillRect(xStart, yStart + (size * 2) + 2, size, size);
-	    canvas.fillRect(xStart, yStart + (size * 3) + 3, size, size);
-	    */
-	    
 	    // Add an array for snake positions (x and y)
 	    int[] x = new int[] { xStart, xStart, xStart, xStart };
 	    int[] y = new int[] { yStart, yStart + size + 1, 
 	    		yStart + (size * 2) + 2, 
 	    		yStart + (size * 3) + 3 };
 	    
-	    canvas.setColor(c);
+	    // Invoke method for exercise 3
+	    drawSnake(canvas, size, x, y);
+	}
+	
+	public void drawSnake(Graphics canvas, int size, int[] x, int[] y) {
+		Color c = Color.GREEN;
+		canvas.setColor(c);
 	    canvas.fillRect(x[0], y[0], size, size);
 	    canvas.fillRect(x[1], y[1], size, size);
 	    canvas.fillRect(x[2], y[2], size, size);
 	    canvas.fillRect(x[3], y[3], size, size);
+	}
+	
+	// Moves the coordinates for snake up
+	public void moveUp(int[] x, int[] y) {
+		// foo
+		
 	}
 }
